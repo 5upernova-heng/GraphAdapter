1ed5baad4d07e6d4ed0d1c5fd1d9bcf7fd527812
Heng Yu <h3ngyu@gmail.com>
2025-05-09 15:53:35 +0800

    Style: format code base

--- finetune_utils.py
+++ finetune_utils.py
@@ -1,18 +1,29 @@
-from sklearn.metrics import roc_auc_score,accuracy_score,average_precision_score
+from sklearn.metrics import roc_auc_score, accuracy_score, average_precision_score
 from transformers import activations
 import torch_geometric as tg
 from torch_geometric.nn.conv import MessagePassing
-from torch_geometric.nn import GATConv,GATv2Conv,SuperGATConv,ResGatedGraphConv,GCN2Conv,GatedGraphConv,SAGEConv
+from torch_geometric.nn import (
+    GATConv,
+    GATv2Conv,
+    SuperGATConv,
+    ResGatedGraphConv,
+    GCN2Conv,
+    GatedGraphConv,
+    SAGEConv,
+)
 from torch_geometric.data import Data
 import numpy as np
 import torch
 import torch.nn.functional as F
-#from utils import load_data
+
+# from utils import load_data
 import pandas as pd
 import argparse
 import joblib
 import random
 from graphadapter import LinearHead
+
+
 def set_random_seed(seed: int = 0):
     """
     set random seed
@@ -26,34 +37,38 @@ def set_random_seed(seed: int = 0):
         torch.cuda.manual_seed_all(seed)
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = False
-    
+
+
 def get_mask(seed):
-    #seed=4
+    # seed=4
     np.random.seed(seed)
-    randint = np.random.randint(0,100,(n,))
-    train_mask = torch.tensor((randint<i)).bool()
-    val_mask = torch.tensor(((randint>=i)&(randint<j))).bool()
-    test_mask = torch.tensor(((randint>=j)&(randint<100))).bool()
-    return train_mask,val_mask,test_mask
+    randint = np.random.randint(0, 100, (n,))
+    train_mask = torch.tensor((randint < i)).bool()
+    val_mask = torch.tensor(((randint >= i) & (randint < j))).bool()
+    test_mask = torch.tensor(((randint >= j) & (randint < 100))).bool()
+    return train_mask, val_mask, test_mask
 
 
-def get_mask(n,i,j,seed):
+def get_mask(n, i, j, seed):
     np.random.seed(seed)
-    randint = np.random.randint(0,100,(n,))
-    train_mask = torch.tensor((randint<i)).bool()
-    val_mask = torch.tensor(((randint>=i)&(randint<j))).bool()
-    test_mask = torch.tensor(((randint>=j)&(randint<100))).bool()
-    return train_mask,val_mask,test_mask
+    randint = np.random.randint(0, 100, (n,))
+    train_mask = torch.tensor((randint < i)).bool()
+    val_mask = torch.tensor(((randint >= i) & (randint < j))).bool()
+    test_mask = torch.tensor(((randint >= j) & (randint < 100))).bool()
+    return train_mask, val_mask, test_mask
+
 
 def normal(x):
-    x = (x-x.mean(dim=0).view(1,-1))/x.std(dim=0).view(1,-1)
+    x = (x - x.mean(dim=0).view(1, -1)) / x.std(dim=0).view(1, -1)
     return x
-def load_data_with_prompt_embedding(dataname,train_ratio,val_ratio,split): ## 
-    if (train_ratio>=100) or (val_ratio+train_ratio>=100):
+
+
+def load_data_with_prompt_embedding(dataname, train_ratio, val_ratio, split):  ##
+    if (train_ratio >= 100) or (val_ratio + train_ratio >= 100):
         raise "train or validation ratio out of 100"
-    x = np.load(f'./token_embedding/{dataname}/sentence_embeddings.npy')
-    edge_index = np.load('./datasets/'+dataname+'/edge_index.npy')
-    y = np.load('./datasets/'+dataname+'/y.npy')
+    x = np.load(f"./token_embedding/{dataname}/sentence_embeddings.npy")
+    edge_index = np.load("./datasets/" + dataname + "/edge_index.npy")
+    y = np.load("./datasets/" + dataname + "/y.npy")
     x = torch.tensor(x).float()
     y = torch.tensor(y).long()
     edge_index = torch.tensor(edge_index).T
@@ -63,85 +78,98 @@ def load_data_with_prompt_embedding(dataname,train_ratio,val_ratio,split): ##
     data = Data()
     data.x = x.float()
     data.y = y
-    if(dataname != 'arxiv'):
-        train_mask,val_mask,test_mask = get_mask(x.shape[0],train_ratio,train_ratio+val_ratio,split)
+    if dataname != "arxiv":
+        train_mask, val_mask, test_mask = get_mask(x.shape[0], train_ratio, train_ratio + val_ratio, split)
     else:
-        train_mask = np.load('./datasets/'+dataname+'/train.npy')
-        val_mask = np.load('./datasets/'+dataname+'/vaild.npy')
-        test_mask = np.load('./datasets/'+dataname+'/test.npy')
+        train_mask = np.load("./datasets/" + dataname + "/train.npy")
+        val_mask = np.load("./datasets/" + dataname + "/vaild.npy")
+        test_mask = np.load("./datasets/" + dataname + "/test.npy")
     data.edge_index = edge_index
     data.train_mask = train_mask
     data.val_mask = val_mask
     data.test_mask = test_mask
     return data
-def evaluate(out,label,mask,metric = 'acc'):
-    if metric == 'roc':
-        py = out[:,1][mask].cpu().numpy()
-        #val = (out[data.val_mask]==data.y[data.val_mask]).sum()
-      #  print(data.y)
+
+
+def evaluate(out, label, mask, metric="acc"):
+    if metric == "roc":
+        py = out[:, 1][mask].cpu().numpy()
+        # val = (out[data.val_mask]==data.y[data.val_mask]).sum()
+        #  print(data.y)
         gy = label[mask].cpu().long().numpy()
-        val = roc_auc_score(gy,py)
+        val = roc_auc_score(gy, py)
         return val
-    elif metric == 'acc':
-        py =  out.max(dim=1)[1][mask].cpu().numpy()
-        #val = (out[data.val_mask]==data.y[data.val_mask]).sum()
-      #  print(data.y)
+    elif metric == "acc":
+        py = out.max(dim=1)[1][mask].cpu().numpy()
+        # val = (out[data.val_mask]==data.y[data.val_mask]).sum()
+        #  print(data.y)
         gy = label[mask].cpu().long().numpy()
-        val = accuracy_score(gy,py)
+        val = accuracy_score(gy, py)
         return val
-    elif metric == 'ap':
-        py = out[:,1][mask].cpu().numpy()
-        #val = (out[data.val_mask]==data.y[data.val_mask]).sum()
-      #  print(data.y)
+    elif metric == "ap":
+        py = out[:, 1][mask].cpu().numpy()
+        # val = (out[data.val_mask]==data.y[data.val_mask]).sum()
+        #  print(data.y)
         gy = label[mask].cpu().long().numpy()
-        val = average_precision_score(gy,py)
+        val = average_precision_score(gy, py)
         return val
-    
-def finetune(data,args):
-    model=None
+
+
+def finetune(data, args):
+    model = None
     device = args.device
-    pretrain_args = joblib.load(f'{args.save_path}model_args.pkl')
-    model = LinearHead(data.x.shape[1],int(data.y.max())+1,pretrain_args)
-    
-    if(args.load_from_pretrain==True):
+    pretrain_args = joblib.load(f"{args.save_path}model_args.pkl")
+    model = LinearHead(data.x.shape[1], int(data.y.max()) + 1, pretrain_args)
+
+    if args.load_from_pretrain == True:
         print("load model from save path")
-        model.ga.load_state_dict(torch.load(f'{args.save_path}save_model_{args.step}.pkl',map_location='cpu'))
-        
-    prompt_x = np.load('./prompt_embedding/'+args.dataset_name+'/prompt_embedding.npy')
-    #prompt_x = np.load('./token_embedding/'+args.dataset_name+'/sentence_embeddings.npy')
+        model.ga.load_state_dict(torch.load(f"{args.save_path}save_model_{args.step}.pkl", map_location="cpu"))
+
+    prompt_x = np.load("./prompt_embedding/" + args.dataset_name + "/prompt_embedding.npy")
+    # prompt_x = np.load('./token_embedding/'+args.dataset_name+'/sentence_embeddings.npy')
     prompt_x = torch.tensor(prompt_x).float().to(device)
-    
-    optimizer = torch.optim.AdamW([
-        {"params":model.lin.parameters(),"lr":args.learning_rate,'weight_decay':1e-3},
-        {"params":model.ga.parameters(),"lr":args.learning_rate,'weight_decay':1e-3},],
-        )
-    
+
+    optimizer = torch.optim.AdamW(
+        [
+            {
+                "params": model.lin.parameters(),
+                "lr": args.learning_rate,
+                "weight_decay": 1e-3,
+            },
+            {
+                "params": model.ga.parameters(),
+                "lr": args.learning_rate,
+                "weight_decay": 1e-3,
+            },
+        ],
+    )
+
     data = data.to(device)
     model = model.to(device)
-    
-    loss=None
+
+    loss = None
     val_acc = 0
     test = 0
-    class_weight = torch.tensor([1,1.0]).to(device)
+    class_weight = torch.tensor([1, 1.0]).to(device)
     for i in range(350):
         model.train()
         model.ga.train()
         optimizer.zero_grad()
-        out,gate = model(data.x,data.edge_index,prompt_x)
-        loss = F.nll_loss(out[data.train_mask],data.y[data.train_mask],weight=class_weight)
+        out, gate = model(data.x, data.edge_index, prompt_x)
+        loss = F.nll_loss(out[data.train_mask], data.y[data.train_mask], weight=class_weight)
         loss.backward()
         optimizer.step()
         with torch.no_grad():
             model.eval()
             model.ga.eval()
-            out,eval_gate= model(data.x,data.edge_index,prompt_x)
-            val = evaluate(out,data.y,data.val_mask,args.metric)
-            if(val>=val_acc):
-                test = evaluate(out,data.y,data.test_mask,args.metric)
-                tr = evaluate(out,data.y,data.train_mask,args.metric)
-                print(f'best {args.metric} in epoch {i}: train:{tr:.4f},valid:{val:.4f},test:{test:.4f}')
-                val_acc=val
-                duration=0
-    print('final_loss',loss.item())
+            out, eval_gate = model(data.x, data.edge_index, prompt_x)
+            val = evaluate(out, data.y, data.val_mask, args.metric)
+            if val >= val_acc:
+                test = evaluate(out, data.y, data.test_mask, args.metric)
+                tr = evaluate(out, data.y, data.train_mask, args.metric)
+                print(f"best {args.metric} in epoch {i}: train:{tr:.4f},valid:{val:.4f},test:{test:.4f}")
+                val_acc = val
+                duration = 0
+    print("final_loss", loss.item())
     model.eval()
     return test
--- prompt_config.py
+++ prompt_config.py
@@ -3,19 +3,19 @@ def get_template_by_dataset(dataset_name):
     ## template_l would used in pretrain
     ## template_r used for infer on downstream task
     ## pretrain and downstream task share same template_l
-    if(dataset_name=='arxiv'):
+    if dataset_name == "arxiv":
         template_l = "Here is a paper published on arXiv. The abstract reads as follows: \n\n"
-        template_r = ".\n\nQuestion: Based on the abstract above, this paper is published on \"___\" subject on Arxiv.\nAnswer: \""
-    elif(dataset_name == 'instagram'):
+        template_r = '.\n\nQuestion: Based on the abstract above, this paper is published on "___" subject on Arxiv.\nAnswer: "'
+    elif dataset_name == "instagram":
         template_l = "Here are an account on Instagram, and its personal profile reads: \n\n"
-        template_r = ".\n\nQuestion: Based on the profile provided , this account is a \"___\" (answer in one word) account on Instagram.\nAnswer: \""
-    elif(dataset_name == 'reddit'):
+        template_r = '.\n\nQuestion: Based on the profile provided , this account is a "___" (answer in one word) account on Instagram.\nAnswer: "'
+    elif dataset_name == "reddit":
         template_l = "It is a user on Reddit, and his last 3 posts are: \n\n"
-        template_r = ".\n\nQuestion: Based on the given posts, the style of this user is \"___\" (answer in one word).\nAnswer: \""
-    elif('update_config' in dataset_name):
+        template_r = '.\n\nQuestion: Based on the given posts, the style of this user is "___" (answer in one word).\nAnswer: "'
+    elif "update_config" in dataset_name:
         template_l = "Here is an intent of network operator, and its description reads: \n\n"
-        template_r = ".\n\nQuestion: Based on the given intents, the modified configuration update is \"___\" (answer in a JSON object).\nAnswer: \""
+        template_r = '.\n\nQuestion: Based on the given intents, the modified configuration update is "___" (answer in a JSON object).\nAnswer: "'
     else:
         raise "template of this dataset are not registered, please modifing the prompt_config.py"
-        
-    return template_l,template_r
+
+    return template_l, template_r
